405ba1f69975bb7f22fcbf7fc33896c0b4898c89
sandersgolf <sanders_mattuk@yahoo.co.uk>
2026-02-23 17:36:23 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = "rgc-scorecard-v7";
+const ASSETS = ["./","./index.html","./manifest.json","./sw.js","./icon-192.png","./icon-256.png","./icon-512.png","./apple-touch-icon.png"];
+self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))).then(()=>self.clients.claim())));
+self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
